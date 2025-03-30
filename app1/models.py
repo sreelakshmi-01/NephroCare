6c1e0cfde6c4a2ce41a593c8e151a63e6d16757f
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-03-30 12:05:21 +0530

    models added

--- app1/models.py
+++ app1/models.py
@@ -42,3 +42,14 @@ class DialysisCenter(models.Model):
 
     def __str__(self):
         return self.name
+
+class Hospital(models.Model):
+    id = models.AutoField(primary_key = True)
+    hosp_name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255, null=True)
+    district = models.CharField(max_length=255)
+    phone_no = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.hosp_name
