005ff0b4be1b6ec247e7d993e022f9f77c47ffa6
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-04-16 14:14:39 +0530

    models edited /

--- app1/models.py
+++ app1/models.py
@@ -80,9 +80,6 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.name}'s Profile"
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created and instance.role == 'user':
