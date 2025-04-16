157b234169ea19ba8a89c8fdfc0f63647d8f64d6
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-04-16 14:14:55 +0530

    models edited imports

--- app1/models.py
+++ app1/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Feature(models.Model):
     title = models.CharField(max_length=255)
