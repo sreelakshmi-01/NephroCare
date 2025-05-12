6c805a6513e0dbedff3a9af2fd4785ca2108e85f
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-05-12 09:27:20 +0530

    cart url

--- app1/urls.py
+++ app1/urls.py
@@ -46,6 +46,8 @@ urlpatterns = [
     path('medicines/', views.medicine_store, name='medicine_store'),
     path('medicine/<int:id>/', views.medicine_detail, name='medicine_details'),
     path('admin-medicine/', views.add_medicine, name='add_medicine'),
+    path('add-to-cart/<str:product_id>/', views.add_to_cart, name='add_to_cart'),
+
 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
