9ad55c3d0f951941b0c1a857e10f5b5c92e2f308
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-05-15 18:38:17 +0530

    logout settings

--- nephrocare/settings.py
+++ nephrocare/settings.py
@@ -113,6 +113,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
